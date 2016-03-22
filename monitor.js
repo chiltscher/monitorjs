7b79bd6711b23e0a10b0d69e5f22a8eaf0a57844
chiltscher <Hiltscher.Christian@Gmail.com>
2016-03-22 11:03:34 +0100

    initial commit
    first commit

---
+++ monitor.js
@@ -0,0 +1,97 @@
+var os = require('os');
+var cpu = require('windows-cpu');
+var toGB = 1073741824
+
+var MonitorClient = function()
+{
+	this.hostname = os.hostname();
+	this.uptime;
+	this.cpu = {};
+
+	var cpus = os.cpus()
+
+	this.cpu.load;
+	this.cpu.cores = cpus.length;
+	this.cpu.speed = cpus.speed;
+	this.cpu.model = cpus.model;
+	this.memory = {}
+	this.memory.totalmem = (os.totalmem() / toGB).toFixed(1);
+	this.memory.freemem;
+	this.memory.usedmem;
+	this.memory.percentage;
+};
+
+MonitorClient.prototype.checkUptime = function()
+{
+	var seconds = os.uptime();
+	var hours = Math.floor(seconds/3600);
+	var minutes = Math.floor((seconds - hours * 3600) / 60 );
+	seconds = Math.floor(seconds - hours * 3600 - minutes * 60);
+	this.uptime = this.adapt(hours) + ":" + this.adapt(minutes) + ":" + this.adapt(seconds); 
+
+};
+
+MonitorClient.prototype.checkMemory = function()
+{
+	var memory = this.memory;
+	memory.freemem = (os.freemem() / toGB).toFixed(1);
+	memory.usedmem = (memory.totalmem - memory.freemem).toFixed(1);
+	memory.percentage = Math.round((100/memory.totalmem) * memory.usedmem);
+};
+
+MonitorClient.prototype.checkLoad = function()
+{
+	var that = this;
+	cpu.totalLoad(
+		function(error, results)
+		{
+			if(error)
+				return console.log(error);
+			that.cpu.load = results
+	});
+};
+
+MonitorClient.prototype.update = function(callback)
+{
+	this.checkLoad();
+	this.checkUptime();
+	this.checkMemory();
+	callback(this);
+};
+
+MonitorClient.prototype.display = function()
+{
+	this.clearScreen();
+	console.log(this.hostname);
+	console.log(this.uptime);
+	console.log(this.memory.usedmem + "/" + this.memory.totalmem + " GB (" + this.memory.percentage + "%)");
+	if (this.cpu.load)
+		console.log(this.cpu.load[0] + " %")
+};
+
+
+// Clear Console function
+MonitorClient.prototype.clearScreen = function()
+{
+	for(var i = 0; i < process.stdout.rows; i++)
+	{
+		console.log('\r\n');
+	}
+};
+
+// Function to set a 0 in front of a one-digit number
+MonitorClient.prototype.adapt = function(number)
+{
+	if (number < 10)
+		return "0"+number
+	else
+		return number+""
+};
+
+var monitor = new MonitorClient();
+var loopID = setInterval(function(){monitor.update(
+	function(monitor)
+	{
+		monitor.display();
+	})}, 500);
+
